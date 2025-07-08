dc818242fb3ba8f31613e02dea69bc20bb7da6df
citeFred <citefred@gmail.com>
2025-07-08 16:04:04 +0900

    feat: Memo 엔터티 생성과 홈컨트롤러 생성

---
+++ src/main/java/com/metaverse/memo/controller/MemoController.java
@@ -0,0 +1,4 @@
+package com.metaverse.memo.controller;
+
+public class MemoController {
+}
---
+++ src/main/java/com/metaverse/memo/domain/Memo.java
@@ -0,0 +1,14 @@
+package com.metaverse.memo.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Memo {
+    private Long id;
+    private String username;
+    private String contents;
+}
