3352632d25f308ecc265f6a4208486d2405c3af2
citeFred <citefred@gmail.com>
2025-07-11 12:31:26 +0900

    feat: 메모 생성 기능 JDBC 소스코드(과거)

--- src/main/java/com/metaverse/memo/controller/MemoController.java
+++ src/main/java/com/metaverse/memo/controller/MemoController.java
@@ -4,8 +4,12 @@ import com.metaverse.memo.domain.Memo;
 import com.metaverse.memo.dto.MemoRequestDto;
 import com.metaverse.memo.dto.MemoResponseDto;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.Collections;
 import java.util.List;
 
@@ -23,51 +27,59 @@ public class MemoController {
         // RequestDto -> Entity 변환
         Memo memo = new Memo(memoRequestDto);
 
-        // (임시) 현재 Memo들의 최대 id 체크
-        Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
-        memo.setId(maxId);
-
         // DB 저장
-        memoList.put(memo.getId(), memo);
+        KeyHolder keyHolder = new GeneratedKeyHolder(); // 기본 키를 반환 받기 위한 객체
+
+        String sql = "INSERT INTO memo (username, contents) VALUES (?, ?)";
+        jdbcTemplate.update( con -> {
+            PreparedStatement preparedStatement = con.prepareStatement( sql, Statement.RETURN_GENERATED_KEYS );
+            preparedStatement.setString(1, memo.getUsername());
+            preparedStatement.setString(2, memo.getContents());
+            return preparedStatement;
+        }, keyHolder);
+
+        // DB INSERT 후 받아온 기본 키 확인
+        Long id = keyHolder.getKey().longValue();
+        memo.setId(id);
 
         // Entity -> ResponseDto 변환
         MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
         return memoResponseDto;
     }
 
-    @GetMapping("/memos")
-    public List<MemoResponseDto> getMemos() {
-        // Map to list
-        List<MemoResponseDto> reponseList = memoList.values().stream()
-                .map(MemoResponseDto::new).toList();
-
-        return reponseList;
-    }
-
-    @PutMapping("/memos/{id}")
-    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto memoRequestDto) {
-        // 해당 id의 메모가 데이터베이스에 존재하는지 확인
-        if (memoList.containsKey(id)) {
-            // 해당 메모 가져오기
-            Memo memo = memoList.get(id);
-
-            // 메모 수정
-            memo.update(memoRequestDto);
-            return memo.getId();
-        } else {
-            throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
-        }
-    }
-
-    @DeleteMapping("/memos/{id}")
-    public Long deleteMemo(@PathVariable Long id) {
-        // 해당 id의 메모가 데이터베이스에 존재하는지 확인
-        if (memoList.containsKey(id)) {
-            // 메모 삭제
-            memoList.remove(id);
-            return id;
-        } else {
-            throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
-        }
-    }
+//    @GetMapping("/memos")
+//    public List<MemoResponseDto> getMemos() {
+//        // Map to list
+//        List<MemoResponseDto> reponseList = memoList.values().stream()
+//                .map(MemoResponseDto::new).toList();
+//
+//        return reponseList;
+//    }
+//
+//    @PutMapping("/memos/{id}")
+//    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto memoRequestDto) {
+//        // 해당 id의 메모가 데이터베이스에 존재하는지 확인
+//        if (memoList.containsKey(id)) {
+//            // 해당 메모 가져오기
+//            Memo memo = memoList.get(id);
+//
+//            // 메모 수정
+//            memo.update(memoRequestDto);
+//            return memo.getId();
+//        } else {
+//            throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
+//        }
+//    }
+//
+//    @DeleteMapping("/memos/{id}")
+//    public Long deleteMemo(@PathVariable Long id) {
+//        // 해당 id의 메모가 데이터베이스에 존재하는지 확인
+//        if (memoList.containsKey(id)) {
+//            // 메모 삭제
+//            memoList.remove(id);
+//            return id;
+//        } else {
+//            throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
+//        }
+//    }
 }
